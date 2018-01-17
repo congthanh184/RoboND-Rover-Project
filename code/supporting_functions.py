366fdc77812bc9c8f11101a9895ed4efc4eaed38
congthanh <congthanh184@gmail.com>
2018-01-17 23:37:21 +0800

    Modify supporting_functions for required information

--- code/supporting_functions.py
+++ code/supporting_functions.py
@@ -50,11 +50,17 @@ def update_rover(Rover, data):
       # Update number of rocks collected
       Rover.samples_collected = Rover.samples_to_find - np.int(data["sample_count"])
 
+      # print('speed =',Rover.vel, 'position =', Rover.pos, 'throttle =', 
+      # Rover.throttle, 'steer_angle =', Rover.steer, 'near_sample:', Rover.near_sample, 
+      # 'picking_up:', data["picking_up"], 'sending pickup:', Rover.send_pickup, 
+      # 'total time:', Rover.total_time, 'samples remaining:', data["sample_count"], 
+      # 'samples collected:', Rover.samples_collected)
+      if Rover.nav_angles is None:
+            nav_angles = 0
+      else:
+            nav_angles = np.mean(Rover.nav_angles * 180/np.pi)
       print('speed =',Rover.vel, 'position =', Rover.pos, 'throttle =', 
-      Rover.throttle, 'steer_angle =', Rover.steer, 'near_sample:', Rover.near_sample, 
-      'picking_up:', data["picking_up"], 'sending pickup:', Rover.send_pickup, 
-      'total time:', Rover.total_time, 'samples remaining:', data["sample_count"], 
-      'samples collected:', Rover.samples_collected)
+      Rover.throttle, 'steer_angle =', Rover.steer, 'yaw = ', Rover.yaw, 'pitch = ', Rover.pitch, 'roll = ', Rover.roll, 'nav_angles = ', nav_angles)
       # Get the current image from the center camera of the rover
       imgString = data["image"]
       image = Image.open(BytesIO(base64.b64decode(imgString)))
